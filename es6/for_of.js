7ea8f00baccb5786aca2465dc584d4623b767f65
Michael Miao <maydaymiao@126.com>
2020-01-06 22:38:20 +0800

    1.6

--- es6/for_of.js
+++ es6/for_of.js
@@ -3,16 +3,16 @@
 // for...in遍历的是key值，可以遍历对象和数组
 // for...of用法和for...in一样，不过遍历的是value值，可以遍历数组，但是不能遍历普通对象
 
-const colors = ['blue', 'green', 'yellow', 'black'];
+const colors = ["blue", "green", "yellow", "black"];
 
 for (let element of colors) {
-    console.log(element) // 只打印数组的value
+  console.log(element); // 只打印数组的value
 }
 
-console.log("==========index和value都获取===========")
+console.log("==========index和value都获取===========");
 // 用到数组解构
 for (let [key, value] of colors.entries()) {
-    console.log(`${key} ${value}`)
+  console.log(`${key} ${value}`);
 }
 // 0 blue
 // 1 green
@@ -20,17 +20,41 @@ for (let [key, value] of colors.entries()) {
 // 3 black
 
 function sum(...args) {
-    let total = 0;
-    for (let i of args) {
-        total += i;
-    }
-    console.log(total)
+  let total = 0;
+  for (let i of args) {
+    total += i;
+  }
+  console.log(total);
 }
 
-sum(1, 2, 3, 4) //正确输出
+sum(1, 2, 3, 4); //正确输出
 
-console.log("==========字符串循环===========")
+console.log("==========字符串循环===========");
 let name = "Michael";
 for (let char of name) {
-    console.log(char)
+  console.log(char);
 }
+
+console.log("==========数组的find方法===========");
+// find()方法返回数组中符合测试函数条件的第一个元素。否则返回undefined
+const inventory = [
+  { name: "apples", quanitity: 2 },
+  { name: "bananas", quanitity: 0 },
+  { name: "cherries", quanitity: 5 }
+];
+
+const bananas = inventory.find(element => element.name == "bananas"); // callback里返回的是布尔值
+console.log(bananas); //{ name: 'bananas', quanitity: 4 }
+
+const bananasIndex = inventory.findIndex(element => element.name == "bananas");
+console.log(bananasIndex); //1
+
+console.log("==========some方法===========");
+// 至少有一个满足条件
+const isEnough = inventory.some(element => element.quanitity > 0);
+console.log(isEnough); // true
+
+console.log("==========every===========");
+// 所有满足条件
+const allEnough = inventory.every(element => element.quanitity > 0);
+console.log(allEnough); // false
